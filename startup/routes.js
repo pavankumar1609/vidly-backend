5ed598217a27516970181380195a1b4f1c33a20c
Pavan Kumar <pavankumarmaddala585@gmail.com>
2022-09-07 12:01:05 +0530

    Added cors.

--- startup/routes.js
+++ startup/routes.js
@@ -7,8 +7,10 @@ const users = require("../routes/users");
 const auth = require("../routes/auth");
 const error = require("../middlewares/error");
 const express = require("express");
+const cors = require("cors");
 
 module.exports = function (app) {
+  app.use(cors());
   app.use(express.json());
   app.use("/api/movies", movies);
   app.use("/api/genres", genres);
